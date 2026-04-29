3201a35d158055cf7c841fcbae5d3ae5240feb45
nancyalgazzar <nancyayman89@gmail.com>
2026-04-29 15:33:14 +0300

    update added

--- src/components/Form/Form.ts
+++ src/components/Form/Form.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { Task, Priority, Category, State } from '../../Model/TaskModel';
 import { FormsModule } from '@angular/forms';
@@ -11,13 +11,27 @@ import { FormsModule } from '@angular/forms';
 })
 export class Form {
   Priority = Priority;
+  @Input() update: boolean = false;
   @Output('task') taskEmitter = new EventEmitter<Task>();
+  @Output() updateTask = new EventEmitter<Task>();
 
   task: Task = new Task();
 
   register() {
     let copy = Object.assign(new Task(), this.task);
     this.task.clearValues();
-    this.taskEmitter.emit(copy);
+    if (!this.update) {
+      this.taskEmitter.emit(copy);
+    } else {
+      this.updateTask.emit(copy);
+    }
+  }
+  @Input() tasksend: Task = new Task();
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    console.log(this.tasksend)
+    if (changes['tasksend'].currentValue) {
+      Object.assign(this.task, this.tasksend);
+    }
   }
 }
--- src/components/TaskCard/TaskCard.ts
+++ src/components/TaskCard/TaskCard.ts
@@ -1,19 +1,28 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task, Priority, Category, State, changeOgj, changeType } from '../../Model/TaskModel';
+import { Form } from '../Form/Form';
 
 @Component({
   templateUrl: './TaskCard.html',
   styleUrl: './TaskCard.css',
   selector: 'app-taskcard',
+  imports: [Form],
 })
 export class TaskCard {
   @Input() task: Task = new Task();
-  @Output() taskEmitter = new EventEmitter();
+  @Output() taskEmitter = new EventEmitter<changeOgj>();
+  updated(t: Task) {
+    this.taskEmitter.emit( {
+      ts: t,
+      ch: changeType.update,
+    });
+  }
   changed(btn: HTMLButtonElement) {
     let c = changeType.update;
     if (Object.values(changeType).includes(btn.innerText.toLowerCase())) {
       c = changeType[btn.innerText.toLowerCase() as keyof typeof changeType];
     }
+
     let obj: changeOgj = {
       ts: this.task,
       ch: c,
@@ -31,5 +40,9 @@ export class TaskCard {
       default:
         return 'bg-light';
     }
+  }
+  show(d: HTMLDivElement) {
+    console.log('focus');
+    d.focus();
   }
 }
